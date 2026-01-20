f29a8c57ffc72fbd278086a2b29034bdfa635f89
Inyoung Cho <cupshop123@gmail.com>
2026-01-21 00:09:05 +0900

    feat: 상세 페이지 레이아웃 설계

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,13 +9,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-dvh bg-[#F9FAFB]">
-        <header className="h-full w-full border-b border-slate-200 bg-white">
-          <div className="mx-auto flex h-15 max-w-300 items-center p-4 sm:p-6 xl:p-6">
+        <header className="h-15 w-full border-b border-slate-200 bg-white">
+          <div className="mx-auto flex h-full max-w-300 items-center p-4 sm:p-6 xl:p-6">
             <GNB />
           </div>
         </header>
 
-        <main className="mx-auto max-w-300 p-4 sm:p-6 xl:p-6">{children}</main>
+        <main>{children}</main>
       </body>
     </html>
   );
